3bc824f83670a5d7340ca6f31f0e0c46c1beed99
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2021-02-07 17:01:06 +0800

    玩家移動完成

---
+++ 3D_FPS/Assets/Script/FPSController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 2000)]
+    public float Speed;
+    [Header("視角靈敏度"), Range(0, 2000)]
+    public float turn;
+
+    private Animator anim;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float v = Input.GetAxis("Vertical"); //前後值
+        float h = Input.GetAxis("Horizontal");
+
+        rb.MovePosition(transform.position + transform.forward * v * Speed * Time.deltaTime + transform.right * h * Speed * Time.deltaTime);
+    }
+}
