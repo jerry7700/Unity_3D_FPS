ddfc5c9400ea3646a087d809e6eba1b633becb9d
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2021-03-07 13:05:56 +0800

    移動完成

--- 3D_FPS/Assets/Script/Enemy.cs
+++ 3D_FPS/Assets/Script/Enemy.cs
@@ -14,11 +14,24 @@ public class Enemy : MonoBehaviour
     /// </summary>
     private NavMeshAgent nav;
 
+    private Animator anim;
+
     [Header("速度"), Range(0f, 30f)]
     public float speed = 2.5f;
 
     [Header("攻擊範圍"),Range(2 , 100)]
     public float rangeAttack = 5f;
+
+    [Header("生成子彈位置")]
+    public Transform Point;
+    [Header("子彈")]
+    public GameObject Bullet;
+    [Header("子彈速度")]
+    public float BulletSpeed;
+    [Header("開槍間隔時間"), Range(0f, 5f)]
+    public float Interval;
+
+    private float timer;
     #endregion
 
     #region 方法
@@ -28,6 +41,7 @@ public class Enemy : MonoBehaviour
     private void Awake()
     {
         Player = GameObject.Find("玩家").transform;
+        anim = GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
         nav.speed = speed;
         nav.stoppingDistance = rangeAttack;
@@ -42,11 +56,34 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         Tack();
+        Fire();
     }
 
     public void Tack()
     {
         nav.SetDestination(Player.position);
+
+        if(nav.remainingDistance > rangeAttack)
+        {
+            anim.SetBool("跑步開關", true);
+        }
+        else
+        {
+            anim.SetBool("跑步開關", false);
+        }
+    }
+
+    public void Fire()
+    {
+        if(timer >= Interval)
+        {
+            timer = 0;
+            Instantiate(Bullet, Point.position, Point.rotation);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
     }
     #endregion
 }
